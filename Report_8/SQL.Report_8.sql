94755ae0b1ccf01054b7eb68455805d6e1d6f6f5
NikitaSidelnikov <sidelnikovn1k1t4@gmail.com>
2021-08-04 14:57:58 +0300

    Iss #5 - Fixing a relationship №8 to №5

--- Report_8/SQL.Report_8.sql
+++ Report_8/SQL.Report_8.sql
@@ -7,6 +7,53 @@
 --SET @DateEnd = '2021-06-01'
 -------------------------------------------------------------------
 
+
+if object_id('tempdb..#tmp_ValidationLog') is not null
+	DROP TABLE #tmp_ValidationLog
+
+CREATE TABLE #tmp_ValidationLog (
+								LogId BIGINT PRIMARY KEY
+								,Incoming BIT
+								,ReceivedOn DATETIME2(7)
+							) --Таблица всех валидных последних логов
+
+INSERT INTO #tmp_ValidationLog  
+	SELECT -- ActualLogs - Id последних валидных логов по каждому обработанному пакету в период отчета
+		ValidationLog.Id		AS LogId
+		,SuccesPackage.Incoming
+		,SuccesPackage.ReceivedOn
+	FROM
+		ValidationLog		
+	INNER JOIN (
+		SELECT -- SuccesPackage = даты последних валидных логов по каждому обработанному пакету в период отчета. Если у пакета несколько логов валидаций, то для каждого запоминаем свой ValidatedOn и максимальный ValidatedOn по пакету. А потом сравним их и отберем те, что равны
+			SuccesLogs.Package		AS PackageId
+			,SuccesLogs.Incoming
+			,SuccesLogs.ValidatedOn
+			,MAX(SuccesLogs.ValidatedOn) OVER (PARTITION BY SuccesLogs.Package)	AS Max_ValidatedOn
+			,SuccesLogs.ReceivedOn	
+		FROM (	
+			SELECT -- SuccesLogs = все валидные логи по всем обработанным пакетам в период отчета
+				ValidationLog.Package
+				,ValidationLog.ValidatedOn
+				,Package.Incoming
+				,Package.ReceivedOn
+			FROM
+				ValidationLog
+			INNER JOIN Package
+				ON Package.Id = ValidationLog.Package
+			WHERE 
+				ValidationLog.Success = 1
+				AND Package.Processed = 1
+				AND Incoming = 0
+				AND ReceivedOn >=  DATETIMEFROMPARTS(DATEPART(YEAR, @DateStart), DATEPART(MONTH, @DateStart), DATEPART(DAY, @DateStart), '0', '0', '0', '0') --начало периода отчета
+				AND ReceivedOn < DATETIMEFROMPARTS(DATEPART(YEAR, @DateEnd), DATEPART(MONTH, @DateEnd), DATEPART(DAY, @DateEnd), '23', '59', '59', '0') --конец периода отчета				
+		) AS SuccesLogs
+	) AS SuccesPackage
+		ON SuccesPackage.PackageId = ValidationLog.Package
+		AND SuccesPackage.Max_ValidatedOn = ValidationLog.ValidatedOn
+	WHERE
+		SuccesPackage.Max_ValidatedOn = SuccesPackage.ValidatedOn
+
 SELECT  --AllCriterionGroupScore  --оценки выбранного лога пакета с учетом всех групп критериев
 	CriterionGroup.Object							AS CriterionGroupName
 	,CriterionGroup.Id								AS CriterionGroupId  
@@ -17,46 +64,27 @@ SELECT  --AllCriterionGroupScore  --оценки выбранного лога пакета с учетом всех 
 	,ScoreLog.AvgScore
 FROM (
 	SELECT --ScoreLog  --средние оценки по каждому критерию всех выбранных логов по выбраному участнику
-		Score.Criterion	
-		,AVG(Score.Value)	AS AvgScore
-	FROM Score
-	INNER JOIN (
-		SELECT -- LastLog - Id последних валидных логов по каждому обработанному пакету
-			ValidationLog.Id AS LogId
+		ScoreGrouped.Criterion	
+		,AVG(ScoreGrouped.Value)	AS AvgScore
+	FROM #tmp_ValidationLog
+	INNER JOIN ( --ScoreGrouped = оценки всех пакетов с удалением дубликатов оценок по критериям
+		SELECT
+			DISTINCT 
+			Score.ValidationLog
+			,Score.Value
+			,Score.MemberGuid
+			--,Criterion.CriterionGroup
+			,Score.Criterion
 		FROM
-			ValidationLog		
-		INNER JOIN (
-			SELECT -- SuccesPackage = даты последних валидных логов по каждому обработанному пакету
-				SuccesLogs.Package				AS PackageId
-				,MAX(SuccesLogs.ValidatedOn)	AS Max_ValidatedOn
-			FROM (	
-				SELECT -- SuccesLogs = все валидные логи по всем обработанным пакетам в период отчета
-					ValidationLog.Package
-					,ValidationLog.ValidatedOn
-				FROM
-					ValidationLog
-				INNER JOIN Package
-					ON Package.Id = ValidationLog.Package
-				WHERE 
-					ValidationLog.Success = 1 --??????
-					AND Package.Processed = 1
-					AND Incoming = 0
-					AND Package.ReceivedOn >=  DATETIMEFROMPARTS(DATEPART(YEAR, @DateStart), DATEPART(MONTH, @DateStart), DATEPART(DAY, @DateStart), '0', '0', '0', '0') --начало периода отчета
-					AND Package.ReceivedOn <= DATETIMEFROMPARTS(DATEPART(YEAR, @DateEnd), DATEPART(MONTH, @DateEnd), DATEPART(DAY, @DateEnd), '23', '59', '59', '0') --конец периода отчета
-			) AS SuccesLogs
-			GROUP BY 
-				SuccesLogs.Package
-		) AS SuccesPackage
-			ON SuccesPackage.PackageId = ValidationLog.Package
-			AND SuccesPackage.Max_ValidatedOn = ValidationLog.ValidatedOn
-	) AS LastLog
-		ON LastLog.LogId = Score.ValidationLog
+			Score 
+	) AS ScoreGrouped
+		ON #tmp_ValidationLog.LogId = ScoreGrouped.ValidationLog
 	INNER JOIN Member
-		ON Member.Guid = Score.MemberGuid
+		ON Member.Guid = ScoreGrouped.MemberGuid
 	WHERE 
 		Member.Guid = @Member
 	GROUP BY
-		Score.Criterion	
+		ScoreGrouped.Criterion	
 ) AS ScoreLog
 RIGHT JOIN Criterion
 	ON Criterion.Code = ScoreLog.Criterion
